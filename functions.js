41d33ea75f1fddc2208fb178c084a32825cb4855
cassonkm <135647400+cassonkm@users.noreply.github.com>
2024-08-18 03:47:36 -0400

    commit

---
+++ functions.js
@@ -0,0 +1,8 @@
+function getInput() {
+    let userInput = document.getElementById("userInput").value;
+    console.log(userInput); // To verify the value was stored
+  }
+
+function add_task(){
+    task.push()
+}
